0c739f46a7ab22b9a11b9f8f0fc1174c669ee976
Stanislav Mekhonoshin <ejabberd@gmail.com>
2017-06-01 22:40:29 +0300

    Add todo

--- lib/omni_scrapper/normalizers/phone.rb
+++ lib/omni_scrapper/normalizers/phone.rb
@@ -4,6 +4,7 @@ module OmniScrapper
   module Normalizers
     class Phone < Base
       def normalized
+        # TODO: raise exception if phone not presetn
         value.scan(/([0-9 \-\(\)]{10,})/).flatten.last.gsub(/[ \-\(\)]/, '')
       end
     end
